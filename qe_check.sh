ceeec21a65164e13cbebf62ac8c36739c0c0165e
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-05-13 17:59:42 +0800

    kunlun

--- qe_check.sh
+++ qe_check.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 set -euo pipefail
 
-awk '
-BEGIN{
-    ORS=" "
-}
-{
-    if ($1=="iteration"){
-        if ($2=="#")
-            print $3;
-        else
-            print substr($2,2)
-    }
-    else if ($1=="total" && $2=="energy")
-        print $4;
-    else if ($1=="estimated")
-        print $5,"\n"
-}' $1
+scf_stdout=${1:-"scf.log"}
+xspectra_stdout=${1:-"xspectra.log"}
 
+if [ -f "$scf_stdout" ];then
+
+    awk '
+    BEGIN{
+        ORS=" "
+    }
+    {
+        if ($1=="iteration"){
+            if ($2=="#")
+                print $3;
+            else
+                print substr($2,2)
+        }
+        else if ($1=="total" && $2=="energy")
+            print $4;
+        else if ($1=="estimated")
+            print $5,"\n"
+    }' $scf_stdout
+
+fi
+
+
+if [ -f "$xspectra_stdout" ];then
+
+    awk '
+    BEGIN{
+        ORS=" "
+    }
+    {
+        if ($2=="k-point")
+            print $2,$4;
+        else if ($4=="converged")
+            print $3,$4,"\n";
+        else if ($3=="CONVERGED")
+            print $3,"\n"
+    }' $xspectra_stdout
+
+fi
