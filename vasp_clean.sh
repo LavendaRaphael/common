2e7f4ad2c641ed78c4ff92ac9cf290f33a5a0b16
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-08-05 16:30:24 +0800

    shtu

--- vasp_clean.sh
+++ vasp_clean.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-  file=(CHG CHGCAR CONTCAR DOSCAR EIGENVAL exts.dat fe.dat IBZKPT INCAR KPOINTS mep.eps movie movie.vasp neb.dat nebef.dat OSZICAR OUTCAR PCDAT POSCAR POSCAR.vasp POSCAR.xyz POTCAR PROCAR REPORT spline.dat stdout STOPCAR tff\* vaspgr vasprun.xml vasp.log vdw_kernel.bindat WAVECAR XDATCAR \*_sub.sh)
-# mode=(0   0      0       0      0        0        0      0      0     0       0       0     0          0       0         0       0      0     0      0           0          0      0      0      0          0      0       0     0      0           0        0                 0       0       0       ) # all
-  mode=(0   0      1       0      0        0        0      0      1     1       0       0     0          0       0         0       0      0     1      0           0          0      0      0      0          1      0       0     0      0           1        0                 0       0       0       ) # posopt
+ filelist=(CHG CHGCAR CONTCAR DOSCAR EIGENVAL exts.dat fe.dat IBZKPT INCAR KPOINTS mep.eps movie movie.vasp neb.dat nebef.dat OSZICAR OUTCAR PCDAT POSCAR POSCAR.vasp POSCAR.xyz POTCAR PROCAR REPORT spline.dat stdout STOPCAR tff\* vaspgr vasprun.xml vasp.log vdw_kernel.bindat WAVECAR XDATCAR \*_sub.sh)
+#    mode=(0   0      0       0      0        0        0      0      0     0       0       0     0          0       0         0       0      0     0      0           0          0      0      0      0          0      0       0     0      0           0        0                 0       0       0       ) # all
+#    mode=(0   0      1       0      0        0        0      0      1     1       0       0     0          0       0         0       0      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # posopt
+     mode=(0   0      0       0      0        0        0      0      1     0       0       0     0          0       0         0       1      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # sch
 
 j=0
 for x in ${mode[@]}
 do
     if [ "$x" == "0" ]
     then
-        rm -rf ${file[$j]}
-        rm -rf 0*/${file[$j]}
-        #rm -rf */${file[$j]}
+        rm -rf ${filelist[$j]}
+        rm -rf 0*/${filelist[$j]}
+        #rm -rf */${filelist[$j]}
     else
-        echo "save $j ${file[$j]}"
+        echo "save $j ${filelist[$j]}"
     fi
     j=$((j+1))
 done
