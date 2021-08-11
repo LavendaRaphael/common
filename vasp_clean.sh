cec1e887f573e83ddc5540b074c6b41f58bd6683
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-08-11 21:17:14 +0800

    jshx

--- vasp_clean.sh
+++ vasp_clean.sh
@@ -4,7 +4,9 @@ set -euo pipefail
  filelist=(CHG CHGCAR CONTCAR DOSCAR EIGENVAL exts.dat fe.dat IBZKPT INCAR KPOINTS mep.eps movie movie.vasp neb.dat nebef.dat OSZICAR OUTCAR PCDAT POSCAR POSCAR.vasp POSCAR.xyz POTCAR PROCAR REPORT spline.dat stdout STOPCAR tff\* vaspgr vasprun.xml vasp.log vdw_kernel.bindat WAVECAR XDATCAR \*_sub.sh)
 #    mode=(0   0      0       0      0        0        0      0      0     0       0       0     0          0       0         0       0      0     0      0           0          0      0      0      0          0      0       0     0      0           0        0                 0       0       0       ) # all
 #    mode=(0   0      1       0      0        0        0      0      1     1       0       0     0          0       0         0       0      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # posopt
-     mode=(0   0      0       0      0        0        0      0      1     0       0       0     0          0       0         0       1      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # sch
+#    mode=(0   0      0       0      0        0        0      0      1     0       0       0     0          0       0         0       1      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # sch
+     mode=(0   0      0       0      0        0        0      0      1     1       0       0     0          0       0         0       0      0     1      0           0          1      0      0      0          0      0       0     0      0           0        0                 0       0       1       ) # init
+
 
 j=0
 for x in ${mode[@]}
