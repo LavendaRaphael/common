9d35181279a0ae83e7d100caf98d2bb72bb7271a
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-08-12 10:39:07 +0800

    jshx

--- vasp_clean.sh
+++ vasp_clean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
- filelist=(CHG CHGCAR CONTCAR DOSCAR EIGENVAL exts.dat fe.dat IBZKPT INCAR KPOINTS mep.eps movie movie.vasp neb.dat nebef.dat OSZICAR OUTCAR PCDAT POSCAR POSCAR.vasp POSCAR.xyz POTCAR PROCAR REPORT spline.dat stdout STOPCAR tff\* vaspgr vasprun.xml vasp.log vdw_kernel.bindat WAVECAR XDATCAR \*_sub.sh)
-#    mode=(0   0      0       0      0        0        0      0      0     0       0       0     0          0       0         0       0      0     0      0           0          0      0      0      0          0      0       0     0      0           0        0                 0       0       0       ) # all
-#    mode=(0   0      1       0      0        0        0      0      1     1       0       0     0          0       0         0       0      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # posopt
-#    mode=(0   0      0       0      0        0        0      0      1     0       0       0     0          0       0         0       1      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # sch
-     mode=(0   0      0       0      0        0        0      0      1     1       0       0     0          0       0         0       0      0     1      0           0          1      0      0      0          0      0       0     0      0           0        0                 0       0       1       ) # init
+ filelist=(CHG CHGCAR CONTCAR DOSCAR EIGENVAL exts.dat fe.dat IBZKPT INCAR KPOINTS mep.eps movie movie.vasp neb.dat nebef.dat nodelist.log OSZICAR OUTCAR PCDAT POSCAR POSCAR.vasp POSCAR.xyz POTCAR PROCAR REPORT spline.dat stdout STOPCAR tff\* vaspgr vasprun.xml vasp.log vdw_kernel.bindat WAVECAR XDATCAR \*_sub.sh)
+#    mode=(0   0      0       0      0        0        0      0      0     0       0       0     0          0       0         0            0       0      0     0      0           0          0      0      0      0          0      0       0     0      0           0        0                 0       0       0       ) # all
+#    mode=(0   0      1       0      0        0        0      0      1     1       0       0     0          0       0         0            0       0      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # posopt
+#    mode=(0   0      0       0      0        0        0      0      1     0       0       0     0          0       0         0            0       1      0     1      0           0          0      0      0      0          0      0       0     0      0           1        0                 0       0       0       ) # sch
+     mode=(0   0      0       0      0        0        0      0      1     1       0       0     0          0       0         0            0       0      0     1      0           0          1      0      0      0          0      0       0     0      0           0        0                 0       0       1       ) # init
 
 
 j=0
