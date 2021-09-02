684137a039c58dce30c7264ad1e46b36540434bc
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-09-02 14:14:29 +0800

    auto

--- git_push.sh
+++ git_push.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
+myusername=lavendaraphael
+
+
 git pull
-git status
 git add .
 git commit -m "auto"
+
+if test -z "$(git status | grep 'git push')"
+then
+    exit
+fi 
+
 myurl=$(git remote -v | tail -n 1 | awk '{print $2}')
-myusername=lavendaraphael
 myurl=https://${myusername}@${myurl: 8}
 git push $myurl
 
