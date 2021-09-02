c2390c6a35839c06f11400330a55d40e465a94de
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-09-02 14:41:09 +0800

    auto

--- git_push.sh
+++ git_push.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+homedir=`find ~ -maxdepth 3 -name "server.me.sh" -print -quit|xargs dirname`/
 set -euo pipefail
 
 myusername=lavendaraphael
 
-
-git pull
-git add .
-git commit -m "auto"
-
-if test -z "$(git status | grep 'git push')"
-then
-    exit
-fi 
-
-myurl=$(git remote -v | tail -n 1 | awk '{print $2}')
-myurl=https://${myusername}@${myurl: 8}
-git push $myurl
-
+cd ${homedir}codes
+array_dir=$(ls)
+for i in ${array_dir}
+do
+    echo $i
+    cd $i
+    git pull
+    git add .
+    git commit -m "auto"
+    if test -n "$(git status | grep 'git push')"
+    then
+        continue
+        myurl=$(git remote -v | tail -n 1 | awk '{print $2}')
+        myurl=https://${myusername}@${myurl: 8}
+        git push $myurl
+    fi
+    cd ..
+done
