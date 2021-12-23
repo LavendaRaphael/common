74898a9e7b7b71336a834d4c32ec03edb1ac9fd6
LavendaRaphael <feifei.tian.cn@gmail.com>
2021-12-23 09:27:31 +0800

    auto

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
 export homedir=`find ~ -maxdepth 3 -name "server.me.sh" -print -quit|xargs dirname`/
 
-source ${homedir}codes/commen/environment.sh
+source ${homedir}codes/common/environment.sh
 source ${homedir}server.me.sh
 source ${homedir}codes/research/common/groupenv.sh
 source ${homedir}codes/research/202103_XasPtO/local_env.sh
