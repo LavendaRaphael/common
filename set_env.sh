33d4de8fa6b39b0733ba9fcd83e6a000e3b16ed0
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-03-23 19:34:50 +0800

    auto

--- set_env.sh
+++ set_env.sh
@@ -4,4 +4,4 @@ source ${homedir}codes/common/environment.sh
 source ${homedir}server.me.sh
 source ${homedir}codes/research/common/groupenv.sh
 source ${homedir}codes/research/202103_XasPtO/local_env.sh
-source ${homedir}codes/research/202112_MDWater/local_env.sh
+source ${homedir}codes/research/202203_MDWater/local_env.sh
